93c855fbbb2fe4fa800c8b95307894254f6612c1
Ryan-Gates <rpgates@suddenlink.net>
2016-11-13 01:10:24 -0600

    Adding options file

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 // more user strings https://github.com/cvandeplas/pystemon/blob/master/user-agents.txt
+
+
+
 var uaStrings = [
    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:41.0) Gecko/20100101 Firefox/41.0",
    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
@@ -7,17 +10,30 @@ var uaStrings = [
 
 var len = uaStrings.length;
 
+var sets = browser.storage.local.get();
+
+var settings = {};
+
+sets.then(function (results) {
+    settings = results;
+}, onError);
+
 function logURL(requestDetails) {
-  console.log("Loading: " + requestDetails.url);
+  //console.log("Loading: " + requestDetails.url);
   //console.log(Math.floor(Math.random()*3));
   var ua = uaStrings[Math.floor(Math.random()*len)];
-  console.log(ua);
+  //console.log(ua);
   
   for (var header of requestDetails.requestHeaders) {
-    if (header.name == "User-Agent") {
-      header.value = ua;
+      //console.log(header.name +' '+header.value);
+      if (header.name == "User-Agent") {
+          if (settings.UseCustom) {
+              header.value = settings.UserString;
+          } else {
+              header.value = ua;
+          }
     } 
-  } 
+  }
   return { requestHeaders: requestDetails.requestHeaders };
   
   
@@ -27,3 +43,37 @@ browser.webRequest.onBeforeSendHeaders.addListener(
   logURL,
         { urls: ["<all_urls>"] },
         ["blocking", "requestHeaders"]);
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function logStorageChange(changes, area) {
+
+    if (area == 'local') {
+
+        console.log(settings);
+
+        var changedItems = Object.keys(changes);
+
+        console.log("Changing local");
+
+        for (item of changedItems) {
+            console.log("Changing local " +item+" "+ settings[item] + " " + changes[item].newValue);
+            settings[item] = changes[item].newValue;
+        }
+    } else {
+        console.log("Change in storage area: " + area);
+
+        var changedItems = Object.keys(changes);
+
+        for (item of changedItems) {
+            console.log(item + " has changed:");
+            console.log("Old value: ");
+            console.log(changes[item].oldValue);
+            console.log("New value: ");
+            console.log(changes[item].newValue);
+        }
+    }
+}
+browser.storage.onChanged.addListener(logStorageChange);
---
+++ options.js
@@ -0,0 +1,28 @@
+﻿function saveOptions(e) {
+    chrome.storage.local.set({
+        UserString: document.querySelector("#UserString").value,
+        UseCustom: document.querySelector("#UseCustom").checked,
+        AllowCookies: document.querySelector("#AllowCookies").checked
+    });
+}
+
+function restoreOptions() {
+
+    function setCurrentChoice(results) {
+        console.log(results);
+        document.querySelector("#UserString").value = results.UserString;
+        document.querySelector("#UseCustom").checked = results.UseCustom;
+        document.querySelector("#AllowCookies").checked = results.AllowCookies;
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    var getting = browser.storage.local.get();
+    getting.then(setCurrentChoice, onError);
+}
+
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
